0135828fb105e8770b3eba7995183ff8e205bd29
mudafiq <pintarngoding@gmail.com>
2022-02-04 10:50:04 +0700

    jawaban ujian guru

--- UjianGuru/Ujian1.java
+++ UjianGuru/Ujian1.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Ujian1 {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Total Belanja: ");
+        int totalbelanja = input.nextInt();
+
+        if (totalbelanja >= 500000 && totalbelanja <= 1000000) {
+            System.out.println("Anda mendapat diskon 10%. Sehingga total yang harus dibayarkan: Rp. "
+                    + (totalbelanja - (totalbelanja * 0.1)));
+        } else if (totalbelanja > 1000000) {
+            System.out.println("Anda mendapat diskon 20%. Sehingga total yang harus dibayarkan: Rp. "
+                    + (totalbelanja - (totalbelanja * 0.2)));
+        } else {
+            System.out.println("Anda tidak mendapat diskon. Silahkan tambah belanja anda sebesar Rp. "
+                    + (500000 - totalbelanja) + " untuk mendapat diskon 10%");
+        }
     }
 }
